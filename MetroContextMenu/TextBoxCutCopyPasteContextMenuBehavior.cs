a3ca8b771fa8569e23c39b18ab1a46801f0508b2
Steven Hollidge <stevenhollidge@hotmail.com>
2012-08-28 21:08:47 +0200

    tidy up

--- MetroContextMenu/TextBoxCutCopyPasteContextMenuBehavior.cs
+++ MetroContextMenu/TextBoxCutCopyPasteContextMenuBehavior.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interactivity;
-using TriggerBase = System.Windows.Interactivity.TriggerBase;
 
 namespace MetroContextMenu
 {
@@ -17,8 +15,7 @@ namespace MetroContextMenu
         public TextBoxCutCopyPasteContextMenuBehavior()
         {
             contextMenu = new ContextMenu();
-            //contextMenu.Style = (Style)Application.Current.Resources["MetroContextMenuStyle"];
-
+            
             cutMenuItem = new MenuItem { Header = "Cut" };
             cutMenuItem.Click += CutClick;
             contextMenu.Items.Add(cutMenuItem);
@@ -89,57 +86,6 @@ namespace MetroContextMenu
             AssociatedObject.MouseRightButtonDown -= AssociatedObject_MouseRightButtonDown;
             AssociatedObject.MouseRightButtonUp -= AssociatedObjectMouseRightButtonUp;
             base.OnDetaching();
-        }
-    }
-
-    public class Behaviors : List<Behavior> { }
-    public class Triggers : List<TriggerBase> { }
-    public static class SupplementaryInteraction
-    {
-        public static Behaviors GetBehaviors(DependencyObject obj)
-        {
-            return (Behaviors)obj.GetValue(BehaviorsProperty);
-        }
-
-        public static void SetBehaviors(DependencyObject obj, Behaviors value)
-        {
-            obj.SetValue(BehaviorsProperty, value);
-        }
-
-        public static readonly DependencyProperty BehaviorsProperty =
-            DependencyProperty.RegisterAttached("Behaviors",
-                                                typeof(Behaviors),
-                                                typeof(SupplementaryInteraction),
-                                                new PropertyMetadata(null, OnPropertyBehaviorsChanged));
-
-        private static void OnPropertyBehaviorsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var behaviors = Interaction.GetBehaviors(d);
-            foreach (var behavior in e.NewValue as Behaviors)
-                behaviors.Add(behavior);
-        }
-
-        public static Triggers GetTriggers(DependencyObject obj)
-        {
-            return (Triggers)obj.GetValue(TriggersProperty);
-        }
-
-        public static void SetTriggers(DependencyObject obj, Triggers value)
-        {
-            obj.SetValue(TriggersProperty, value);
-        }
-
-        public static readonly DependencyProperty TriggersProperty =
-            DependencyProperty.RegisterAttached("Triggers",
-                                                typeof(Triggers),
-                                                typeof(SupplementaryInteraction),
-                                                new PropertyMetadata(null, OnPropertyTriggersChanged));
-
-        private static void OnPropertyTriggersChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var triggers = Interaction.GetTriggers(d);
-            foreach (var trigger in e.NewValue as Triggers)
-                triggers.Add(trigger);
         }
     }
 }
